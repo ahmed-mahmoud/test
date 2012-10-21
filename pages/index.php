e1c4e345bcc3ffa544b38fb9a3640fc4236c6225
AHMED <ahmed@x.zero4ahmed@gmail.com>
2012-10-21 08:25:42 +0200

    add pages directory and index.php

---
+++ pages/index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Created on Oct 21, 2012
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+ echo 'Hello';
+?>
